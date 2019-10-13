97f209e0d8e52297cd717a9b877a6ed0f4e68391
yanyanshan <2190907815@qq.com>
2019-10-13 18:53:14 +0800

    feat:webpack的学习笔记

---
+++ webpack/11笔记/src/js/index.js
@@ -0,0 +1,11 @@
+import '../css/style.css';
+import '../css/bootstrap.css';
+import $ from 'jquery'
+
+$("div").css("background","red");
+
+$("p").css("color","blue");
+
+let a = 10;
+
+console.log(a);
